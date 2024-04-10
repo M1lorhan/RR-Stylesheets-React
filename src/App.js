c8e33dc606ee2ca4bbf4dade5dc14f01e77d9658
Marshall Lorhan <mlorhan@gmail.com>
2024-04-09 20:18:03 -0700

    added styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 
 function App() {
+
+  
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Welcome to Myrhiam's Flower Business</h1>
+      <Flower name="dalia" price={3.87} color={"red"}/>
+      <Flower name="sun Flower" price={6.5} color={"orange"}/>
+      <Flower name="rose" price={1.23} color={"pink"}/>
+      <Flower name="daisy" price={5.4} color={"yellow"}/>
     </div>
   );
 }
+
+const expensiveStyle = {fontWeight: "500", color: "red", textDecorationLine: "underline"}
+const onSaleStyle = {color: "green"}
+
+const Flower = (props) => (
+  <div>
+    <h2 style={{color: props.color}}>{props.name}</h2>
+    <h4 style={props.price >= 5 ? expensiveStyle : onSaleStyle}>{props.price}</h4>
+
+  </div>
+
+)
 
 export default App;
